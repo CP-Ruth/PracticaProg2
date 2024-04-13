c7d71a113b6e551dde4089030c08de670d0acd1e
Ruth <elith.tati@gmail.com>
2024-04-13 16:00:18 -0300

    Changes by Dell

--- src/TP2/Biblioteca.java
+++ src/TP2/Biblioteca.java
@@ -1,4 +1,32 @@
 package TP2;
 
+import java.util.ArrayList;
+
 public class Biblioteca {
+        private String nombre;
+        private ArrayList<Libro> libros = new ArrayList<>();
+
+                //Constructor
+        public Biblioteca(String nombre) {
+                this.nombre = nombre;
+        }
+
+                //GETTER and SETTER
+        public String getNombre() {
+                return nombre;
+        }
+
+        public void setNombre(String nombre) {
+                this.nombre = nombre;
+        }
+
+        public ArrayList<Libro> getLibros() {
+                return libros;
+        }
+
+        public void setLibros(ArrayList<Libro> libros) {
+                this.libros = libros;
+        }
+
+
 }
